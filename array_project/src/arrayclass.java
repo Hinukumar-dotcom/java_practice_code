f07b555939461b653f1c38fc6636e8a7122c933a
Hinukumar Medavarapu <hinukumar.medavarapu@suresby.net>
2020-07-24 17:31:20 +0530

    First commit

---
+++ array_project/src/arrayclass.java
@@ -0,0 +1,40 @@
+
+public class arrayclass {
+	public static void main(String []args) {
+		System.out.println("Below function is defined as array initializer using While loop :");
+		System.out.println();
+		
+		/*
+		int apple[] = new int[5];
+		apple[0] = 10;
+		apple[1] = 20;
+		apple[2] = 30;
+		apple[3] = 40;
+		apple[4] = 50;
+		System.out.println("Index value 1 is :" + apple[1]);
+		*/
+		int x=0;
+		int apple[] = {10,20,30,40,50,60,70,80,90,100};
+		
+		while(x<=9) {
+			System.out.println(apple[x]);
+			x++;
+		}
+		
+		
+		/*
+		System.out.println(apple[0]);
+		System.out.println(apple[1]);
+		System.out.println(apple[2]);
+		System.out.println(apple[3]);
+		System.out.println(apple[4]);
+		System.out.println(apple[5]);
+		System.out.println(apple[6]);
+		System.out.println(apple[7]);
+		System.out.println(apple[8]);
+		System.out.println(apple[9]);
+		*/
+	}
+}
+
+
