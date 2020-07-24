c575ee3e3408ea0ad13c3453754a4ca440f1b114
Hinukumar Medavarapu <hinukumar.medavarapu@suresby.net>
2020-07-24 18:14:06 +0530

    code modified Enhanced_for_loop

--- array_project/src/arrayclass.java
+++ array_project/src/arrayclass.java
@@ -3,7 +3,7 @@ public class arrayclass {
 	public static void main(String []args) {
 		//System.out.println("Below function is defined as array initializer using While loop :");
 		//System.out.println();
-		System.out.println("Index\tvalue");
+		//System.out.println("Index\tvalue");
 		/*
 		int apple[] = new int[5];
 		apple[0] = 10;
@@ -13,13 +13,16 @@ public class arrayclass {
 		apple[4] = 50;
 		System.out.println("Index value 1 is :" + apple[1]);
 		*/
-		int x=0;
+		//int x =0;
+		int total=0;
+		System.out.println("Using Enhanced for loop for adding nos defined in array :");
 		int apple[] = {10,20,30,40,50,60,70,80,90,100};
 		
-		while(x<apple.length) {
-			System.out.println(x +"\t" +apple[x]);
-			x++;
+		for(int x : apple) {
+			//System.out.println(x +"\t" +apple[x]);
+			total += x;
 		}
+		System.out.println("The total value of Array is :"+ total);
 		/*
 		System.out.println(apple[0]);
 		System.out.println(apple[1]);
