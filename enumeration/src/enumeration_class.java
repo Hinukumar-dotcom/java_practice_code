c8708a72c9572d8c6f4587ee287ef8bc57d62984
Hinukumar Medavarapu <hinukumar.medavarapu@suresby.net>
2020-08-09 21:29:02 +0530

    Space method added

--- enumeration/src/enumeration_class.java
+++ enumeration/src/enumeration_class.java
@@ -3,6 +3,7 @@ public class enumeration_class {
 	public static void main(String []args) {
 	for (dummy users : dummy.values()) {
 		System.out.printf("Name : %s,Desc : %s,Age :%s\n",users,users.getDesc(),users.getAge());
+		System.out.println();
 	}
 
 }
